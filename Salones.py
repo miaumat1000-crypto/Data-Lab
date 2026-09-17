a4c3746f83fc7200143c598e10f743b751dcfee3
Campus Sala 101 <casala5@usantotomas.red>
2026-09-17 11:13:41 -0500

    Validacion de semana 3

---
+++ Salones.py
@@ -0,0 +1,10 @@
+numero_registro = int(input("Digite su número de registro: "))
+
+if 1 <= numero_registro <= 5:
+    print("Pertenece al Salón A")
+elif 6 <= numero_registro <= 11:
+    print("Pertenece al Salón B")
+else:
+    print("No pertenece")
+    
+    
